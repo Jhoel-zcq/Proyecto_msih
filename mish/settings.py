3dcb78d882c351c484d4b2a22c728e14048dc7a0
jhoel <jcanaviri@usm.cl>
2025-10-10 14:45:25 -0300

    Eliminar archivos .pyc y actualizar gitignore

--- mish/settings.py
+++ mish/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -124,3 +124,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
--- prueba/views.py
+++ prueba/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from random import choice
+from .forms import PostForm
 # Create your views here.
 
 #paginas html
@@ -20,6 +22,47 @@ def funcion_sandbox(request):
 
 def funcion_ejercitar(request): 
     return render(request,"prueba/funcion_ejercitar.html")
+#FUNCIONES
+def pagina_aleatoria_fis100(request):
+    paginas = ['Tiempo_y_distancia','Medición','Rapidez_de_cambio','Vectores','Triangulo_vectorial','Descripción_de_movimiento','Fuerzas_y_leyes_de_Newton']
+    
+    return redirect(choice(paginas))
+
+def pagina_aleatoria_fis111(request):
+    paginas=['Cinematica','Principios_de_Newton','Trabajo_y_energia']
+    return redirect(choice(paginas))
+
+#FIS100
+def Tiempo_y_distancia(request):
+    return render(request,"prueba/fis100/Tiempo_y_distancia.html")
+
+def Medición(request):
+    return render(request, "prueba/fis100/Medición.html")
+
+def Rapidez_de_cambio(request):
+    return render(request,"prueba/fis100/Rapidez_de_cambio.html")
+
+def Vectores(request):
+    return render(request,"prueba/fis100/Vectores.html")
+
+def Triangulo_vectorial(request):
+    return render(request,"prueba/fis100/Triangulo_vectorial.html")
+
+def Descripción_de_movimiento(request):
+    return render(request,"prueba/fis100/Descripción_de_movimiento.html" )
+
+def Fuerzas_y_leyes_de_Newton(request):
+    return render(request,"prueba/fis100/Fuerzas_y_leyes_de_Newton.html")
+
+#FIS111
+def Cinematica(request):
+    return render(request,"prueba/fis111/Cinematica.html")
+
+def Principios_de_Newton(request):
+    return render(request,"prueba/fis111/Principios_de_Newton.html")
+
+def Trabajo_y_energia(request):
+    return render(request, "prueba/fis111/Trabajo_y_energia.html")
 
 # blog/views.py
 from .models import Post
@@ -29,7 +72,7 @@ def lista_posts(request):#es el inicio
     return render(request, 'prueba/lista_posts.html', {'posts': posts})
 
 # vista para crear un post
-from .forms import PostForm
+
 
 def crear_post(request):
     if request.method == 'POST':
