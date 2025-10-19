1df6d02d9bc9086feaf8dc4edbd6cefba94a4f37
jhoel <jcanaviri@usm.cl>
2025-10-19 20:32:15 -0300

    commit 4, solo para configurar el gitignore

--- prueba/forms.py
+++
@@ -1,8 +0,0 @@
-# blog/forms.py
-from django import forms
-from .models import Post
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post  # Modelo asociado
-        fields = ['titulo', 'contenido']  # Campos que el usuario puede llenar
